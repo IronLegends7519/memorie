76fc4ae82cfaa150061374aced1ae33703907372
IronLegends7519 <drafin@edenschool.fr>
2024-01-12 14:18:07 +0100

    peut cliquer

---
+++ main.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.cards')
+
+  for(let i= 0 ;i < cards.length ; i++ ){
+    cards[i].addEventListener('click',()=>{
+      console.log('hello');
+    })
+  
+  };
