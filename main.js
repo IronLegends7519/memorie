b6efb2f8070783570f3ec4965ac0fe778ca9030f
IronLegends7519 <drafin@edenschool.fr>
2024-01-15 15:22:43 +0100

    Random null

--- main.js
+++ main.js
@@ -3,8 +3,19 @@ let a = "a";
 let b = "b";
 let c = "c";
 let d = "d";
+let Random;
+let T = cards;
+let A = [];
+let C;
+let B = [];
+let D = T;
 
   for(let i= 0 ;i < cards.length ; i++ ){
+    Random=Math.floor(Math.random(1)*T.length);
+    B = T[i].textContent
+    A.push(D[Random]);
+    
+    cards[i].textContent = A[i].textContent
     cards[i].addEventListener('click',()=>{
       if(cards[i].style.backgroundColor === 'black'){
         cards[i].style.backgroundColor = 'white';
@@ -19,20 +30,21 @@ let d = "d";
         d = cards[i]
         console.log(b)
       }
-      else if(a !== cards[i].textContent && b !==cards[i].textContent && a !== b){
+      else if(a !== b){
         c.style.backgroundColor = 'black';
         a = cards[i].textContent;
+        c = cards[i]
         console.log(a)
         d.style.backgroundColor = 'black';
         b ="b"
+        d="d"
       }
       else if(a === b){
-        a="a"
+        a=cards[i].textContent
+        c=cards[i]
         b="b"
+        d="d"
         console.log('bonjour')
-      }
-
-
-        
+      }   
       }
     })}
