ca9ad76b56915a585e88f784592b2837dc862835
IronLegends7519 <drafin@edenschool.fr>
2024-01-15 17:18:23 +0100

    finish random

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-const cards = document.querySelectorAll('.cards')
+const cards = document.querySelectorAll('.cards');
 let a = "a";
 let b = "b";
 let c = "c";
 let d = "d";
-let Random;
-let T = cards;
-let A = [];
-let C;
-let B = [];
-let D = T;
+let cardsMixedArray = [];
 
-  for(let i= 0 ;i < cards.length ; i++ ){
-    Random=Math.floor(Math.random(1)*T.length);
-    B = T[i].textContent
-    A.push(D[Random]);
-    
-    cards[i].textContent = A[i].textContent
+let cardsArray = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+while(cardsArray.length > 0){
+  let random = Math.floor(Math.random() * cardsArray.length);
+  cardsMixedArray.push(cardsArray[random]);
+  cardsArray = cardsArray.slice(0,random).concat(cardsArray.slice(random+1));
+}
+console.log(cardsMixedArray)
+
+
+    for(let i= 0 ;i < cards.length ; i++ ){
     cards[i].addEventListener('click',()=>{
       if(cards[i].style.backgroundColor === 'black'){
         cards[i].style.backgroundColor = 'white';
@@ -45,6 +44,6 @@ let D = T;
         b="b"
         d="d"
         console.log('bonjour')
-      }   
+      }
       }
     })}
